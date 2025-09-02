4d123e1897e7d8d2a2468faefe25e49164f798ea
Leo Ledlow <jledlow22@gmail.com>
2025-09-02 17:39:31 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { App } from "@slack/bolt";
+
+const app = new App({
+  appToken: process.env.SLACK_APP_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+});
+
+(async () => {
+  await app.start();
+  console.log("⚡️ Bolt app started");
+})();
+
+app.event("app_mention", async ({ event, context }) => {
+  try {
+    await app.client.chat.postMessage({
+      channel: event.channel,
+      text: `Hello <@${event.user}>! How can I assist you today?`,
+      token: context.botToken,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
