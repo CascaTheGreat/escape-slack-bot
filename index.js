787d53a24b253d90f733db9ac23a43f6155f595f
Leo Ledlow <jledlow22@gmail.com>
2025-09-02 18:17:07 -0400

    Added progress check

--- index.js
+++ index.js
@@ -14,11 +14,34 @@ const app = new App({
 
 app.event("app_mention", async ({ event, context }) => {
   try {
-    await app.client.chat.postMessage({
-      channel: event.channel,
-      text: `Hello <@${event.user}>! How can I assist you today?`,
-      token: context.botToken,
-    });
+    if (event.text.includes("escapees")) {
+      //make API call to Eventbrite
+      const response = await fetch(
+        `https://www.eventbriteapi.com/v3/events/${process.env.EVENTBRITE_EVENT_ID}/capacity_tier/`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.EVENTBRITE_API_TOKEN}`,
+          },
+        }
+      );
+      const data = await response.json();
+      const quantitySold = data.quantity_sold;
+      //creates a progress bar with the 🟩 and ⬜ emojis, 20 total emojis with a cap of 350
+      const progress = Math.min(Math.floor((quantitySold / 350) * 20), 20);
+      const progressBar = "🟩".repeat(progress) + "⬜".repeat(20 - progress);
+
+      await app.client.chat.postMessage({
+        channel: event.channel,
+        text: `We've registered ${quantitySold} attendees for the event.\n${progressBar}`,
+        token: context.botToken,
+      });
+    } else {
+      await app.client.chat.postMessage({
+        channel: event.channel,
+        text: `Hello <@${event.user}>! How can I assist you today?`,
+        token: context.botToken,
+      });
+    }
   } catch (error) {
     console.error(error);
   }
