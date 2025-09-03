8cb730c8eb3509c63d99916ab61c1edfb15736ee
Leo Ledlow <jledlow22@gmail.com>
2025-09-03 15:14:09 -0400

    Updated for event aggregation

--- index.js
+++ index.js
@@ -35,6 +35,78 @@ app.event("app_mention", async ({ event, context }) => {
         text: `We've registered ${quantitySold} attendees for the event.\n${progressBar}`,
         token: context.botToken,
       });
+    }
+    if (event.text.includes("retreats")) {
+      //make API call to Eventbrite with pagination
+      const page = "";
+      const per_page = 50;
+      let has_more = true;
+      const dates = {
+        "Saturday Sept. 27 - Sunday, Sept. 28": {
+          num_attendees: 0,
+          team: "Star",
+          emoji: "⭐",
+        },
+        "Friday, Sept. 26 - Saturday, Sept. 27": {
+          num_attendees: 0,
+          team: "Hero",
+          emoji: "⚔️",
+        },
+        "Friday, Oct. 24 - Saturday, Oct. 25 ": {
+          num_attendees: 0,
+          team: "Star",
+          emoji: "⭐",
+        },
+        "Saturday, Oct. 25 - Sunday, Oct. 26": {
+          num_attendees: 0,
+          team: "Distance",
+          emoji: "🏃",
+        },
+        "Friday, Nov. 14 - Saturday, Nov. 15 (Transfer Retreat)": {
+          num_attendees: 0,
+          team: "Distance",
+          emoji: "🏃",
+        },
+        "Saturday, Nov. 15 - Sunday, Nov. 16": {
+          num_attendees: 0,
+          team: "Hero",
+          emoji: "⚔️",
+        },
+      };
+      while (has_more) {
+        const response = await fetch(
+          `https://www.eventbriteapi.com/v3/events/${process.env.EVENTBRITE_EVENT_ID}/attendees/?continuation=${page}&page_size=${per_page}`,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.EVENTBRITE_API_TOKEN}`,
+            },
+          }
+        );
+        const data = await response.json();
+
+        for (const attendee of data.attendees) {
+          const retreatDate = attendee.answers[0].answer;
+          if (dates[retreatDate]) {
+            dates[retreatDate].num_attendees++;
+          }
+        }
+
+        has_more = data.has_more_items;
+        page = data.pagination.continuation;
+      }
+
+      await app.client.chat.postMessage({
+        channel: event.channel,
+        text: `Here are the current retreat registrations:\n${Object.entries(
+          dates
+        )
+          .map(
+            ([date, info]) =>
+              `${date} (${info.emoji} ${info.team}): *${info.num_attendees}*`
+          )
+          .join("\n")}`,
+        token: context.botToken,
+      });
     } else {
       await app.client.chat.postMessage({
         channel: event.channel,
